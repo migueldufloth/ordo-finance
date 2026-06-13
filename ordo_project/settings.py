12cae9da368eb210e8738ca6195d8505d462dc9d
Miguel <migueldufloth@gmail.com>
2026-06-12 22:25:00 -0300

    chore: remove microserviço FastAPI e limpa configurações relacionadas

--- ordo_project/settings.py
+++ ordo_project/settings.py
@@ -110,9 +110,6 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
 
-# URL interna do microserviço FastAPI (http://api:8000 no Docker)
-REPORTS_API_URL = os.environ.get('REPORTS_API_URL', 'http://api:8000')
-
 # Sentry — monitoramento de erros em produção
 # Só ativa se SENTRY_DSN estiver definido no ambiente
 _SENTRY_DSN = os.environ.get('SENTRY_DSN', '')
